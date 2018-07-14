207ce9a9a0165b1c412338a748d6419f508d935b
Daniel Molkentin <daniel@molkentin.de>
2018-07-15 00:52:03 +0200

    Kalender: Kommende 5 Termine über max. 14 Tage anzeigen

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 hugo
 tools/merge_cals.py
-upcoming="$(tools/gen_upcoming.py static/all.ics 7 7|tr '\n' ' ')"
+upcoming="$(tools/gen_upcoming.py static/all.ics 14 5|tr '\n' ' ')"
 sed -i "s#CALENDAR#$upcoming#g" public/index.html
