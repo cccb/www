dec4fce900820f868a5d0c0926998083e0a127db
Marek Krug <git-mail@marekkrug.de>
2025-02-11 23:12:21 +0100

    removed test code and removed filmabend

--- build.sh
+++ build.sh
@@ -3,9 +3,5 @@
 hugo $(cat .hugo-params)
 ./tools/merge_cals.py
 upcoming="$(tools/gen_upcoming.py static/all.ics 20 5 | tr '\n' ' ')"
-echo $upcoming
-cat public/index.html
 cp static/all.ics public/all.ics
 sed -i "s#CALENDAR#$upcoming#g" public/index.html
-cat public/index.html
-echo $upcoming
